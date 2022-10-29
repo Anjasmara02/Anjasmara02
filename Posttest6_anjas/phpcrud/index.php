8e627a6c37259d8d8d5d9fbe7f5d98d55b123c2e
Anjasmara02 <114128325+Anjasmara02@users.noreply.github.com>
2022-10-29 19:34:06 +0800

    Add files via upload

---
+++ Posttest6_anjas/phpcrud/index.php
@@ -0,0 +1,14 @@
+<?php
+include 'functions.php';
+
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>HALLO SAYA ANJASMARA</p>
+	<P> HAI </P>
+</div>
+
+<?=template_footer()?>
